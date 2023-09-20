963305b3a3f2a491a33adabd9e061eafdef21d53
bdehapiot <b.dehapiot@gmail.com>
2023-09-20 16:31:01 +0200

    update

--- init.py
+++ init.py
@@ -11,5 +11,20 @@ ROOT_PATH = Path(__file__).resolve().parents[0]
 REPO_NAME = ROOT_PATH.name
 ENV_NAME = REPO_NAME.split("_", 1)[1] if "_" in REPO_NAME else REPO_NAME
 
-print(REPO_NAME)
-print(ENV_NAME)
+#%% Replace placeholders ------------------------------------------------------
+
+def replace_placeholders(file_path):
+    
+    # Read file
+    with open(file_path, "r") as file:
+        txt = file.read()
+
+    # Replace placeholders
+    txt = txt.replace("{{ env_name }}", ENV_NAME)
+    txt = txt.replace("{{ env_name }}", ENV_NAME)
+
+    # Update file
+    with open(file_path, "w") as file:
+        file.write(txt)
+
+replace_placeholders(Path(ROOT_PATH / "environment.yml"))
