98ff89e8a84433c799778814c652d2a5834b868f
bdehapiot <b.dehapiot@gmail.com>
2023-09-21 15:18:55 +0200

    update

---
+++ utils/README_template.md
@@ -0,0 +1,15 @@
+![Author Badge](https://img.shields.io/badge/Author-Benoit_Dehapiot-blue?labelColor=rgb(50%2C60%2C65)&color=rgb(149%2C157%2C165))  
+![License Badge](https://img.shields.io/badge/Licence-GNU_General_Public_License_v3.0-blue?labelColor=rgb(50%2C60%2C65)&color=rgb(149%2C157%2C165))  
+![Python Badge](https://img.shields.io/badge/Python-{{ python_version }}-blue?logo=python&logoColor=rgb(149%2C157%2C165)&labelColor=rgb(50%2C60%2C65))
+
+# {{ repo_name }}
+
+## Description
+
+## Installation
+
+## Dependencies
+### Conda
+{{ conda_dependencies }}
+### pip
+{{ pip_dependencies }}
---
+++ utils/repo_update.py
@@ -0,0 +1,56 @@
+#%% Imports -------------------------------------------------------------------
+
+import re
+import yaml
+from pathlib import Path
+
+#%% Initialize ----------------------------------------------------------------
+
+root_path = Path(__file__).resolve().parents[1]
+repo_name = root_path.name
+
+#%% Extract relevant info -----------------------------------------------------
+
+# Read environment.yml
+with open(Path(root_path / "environment.yml"), 'r') as file:
+    yml = yaml.safe_load(file)
+
+# Extract environment name
+env_name = yml.get('name', '')
+
+# Extract conda and pip dependencies
+conda_dependencies = []
+pip_dependencies = []
+for dependency in yml.get('dependencies', []):
+    if isinstance(dependency, str):
+        conda_dependencies.append(dependency)
+        if dependency.startswith('python='):
+            python_version = dependency.split('=')[1]
+    elif isinstance(dependency, dict) and 'pip' in dependency:
+        pip_dependencies.extend(dependency['pip'])
+
+# Format conda and pip dependencies
+conda_dependencies_str = ""
+for dependency in conda_dependencies:
+    conda_dependencies_str += f"- {dependency}\n"
+pip_dependencies_str = ""
+for dependency in pip_dependencies:
+    pip_dependencies_str += f"- {dependency}\n"
+print(pip_dependencies_str)
+
+#%% Update README -------------------------------------------------------------
+
+# Read README_template.md
+with open(Path(root_path / "utils" / "README_template.md"), "r") as file:
+    txt = file.read()
+
+# Replace placeholders
+txt = txt.replace("{{ repo_name }}", repo_name)
+txt = txt.replace("{{ env_name }}", env_name)
+txt = txt.replace("{{ python_version }}", python_version)
+txt = txt.replace("{{ conda_dependencies }}", conda_dependencies_str)
+txt = txt.replace("{{ pip_dependencies }}", pip_dependencies_str)
+
+# Write README.md in root path
+with open(Path(root_path / "README.md"), "w") as file:
+    file.write(txt)
